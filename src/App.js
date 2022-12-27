630dbd9cb2dedb7b93fc01fa307216ffd64465a1
Meka Seymour <mekaseymour@gmail.com>
2022-12-27 09:39:40 -0500

    Second commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <div className="content">
+          <h1 className="header">If money has you stressed, we feel you.</h1>
+        <div className="subheader">
+          <p>Together we’re talking about how money makes us feel and getting answers to the important questions from financial coaches and financial therapists.</p>
+          <p>Lets get into it 💪</p>
+        </div>
+        <a className="button" href="https://google.com" target="_blank" rel="noreferrer">Join the community</a>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
