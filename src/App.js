90421a8b4e81f83ee6d3d259ae4019e507de22f9
Meka Seymour <mekaseymour@gmail.com>
2022-12-28 02:47:01 -0500

    Add typeform link

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
         <div className="content">
           <h1 className="header">If money has you stressed, we feel you.</h1>
         <div className="subheader">
-          <p>Together we’re talking about how money makes us feel and getting answers to the important questions from financial coaches and financial therapists.</p>
+          <p>Together we’re talking about our feelings around money and getting answers to the important questions from financial therapists and coaches.</p>
           <p>Let's get into it 💪</p>
         </div>
-        <a className="button" href="https://google.com" target="_blank" rel="noreferrer">Join the community</a>
+        <a className="button" href="https://r6cx6cdnhu5.typeform.com/to/OgSPT2I3" target="_blank" rel="noreferrer">Join the community</a>
         </div>
       </header>
     </div>
