8edba4f36ee257bb6cab233ee9906fd946292327
Meka Seymour <mekaseymour@gmail.com>
2023-01-09 21:12:27 -0500

    Coming soon text

--- src/App.js
+++ src/App.js
@@ -1,5 +1,22 @@
 import './App.css';
 
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <div className="content">
+//           <h1 className="header">If money has you stressed, we feel you.</h1>
+//         <div className="subheader">
+//           <p>Together we’re talking about our feelings around money and getting answers to the important questions from financial therapists and coaches.</p>
+//           <p>Let's get into it 💪</p>
+//         </div>
+//         <a className="button" href="https://r6cx6cdnhu5.typeform.com/to/OgSPT2I3" target="_blank" rel="noreferrer">Join the community</a>
+//         </div>
+//       </header>
+//     </div>
+//   );
+// }
+
 function App() {
   return (
     <div className="App">
@@ -7,10 +24,8 @@ function App() {
         <div className="content">
           <h1 className="header">If money has you stressed, we feel you.</h1>
         <div className="subheader">
-          <p>Together we’re talking about our feelings around money and getting answers to the important questions from financial therapists and coaches.</p>
-          <p>Let's get into it 💪</p>
+          <p>Coming soon 🚧</p>
         </div>
-        <a className="button" href="https://r6cx6cdnhu5.typeform.com/to/OgSPT2I3" target="_blank" rel="noreferrer">Join the community</a>
         </div>
       </header>
     </div>
